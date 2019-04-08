cde3499d23c500654dead7f55798332a6de7a5f6
Sam Harwood <support@netguard.io>
2019-04-08 17:52:13 +0200

    init

---
+++ NETLicense.IO exemple/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace NETLicense.IO_exemple {
+    partial class Form1 {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_cipher = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txt_decrypted = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt_result = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Encrypt message";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hello and Welcome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.label2.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label2.Location = new System.Drawing.Point(16, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "This is netlicense.io exemple app in c#";
+            // 
+            // txt_cipher
+            // 
+            this.txt_cipher.Location = new System.Drawing.Point(147, 18);
+            this.txt_cipher.Name = "txt_cipher";
+            this.txt_cipher.Size = new System.Drawing.Size(158, 20);
+            this.txt_cipher.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.txt_decrypted);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.txt_result);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txt_cipher);
+            this.panel1.Location = new System.Drawing.Point(6, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(325, 136);
+            this.panel1.TabIndex = 4;
+            // 
+            // txt_decrypted
+            // 
+            this.txt_decrypted.Enabled = false;
+            this.txt_decrypted.Location = new System.Drawing.Point(147, 95);
+            this.txt_decrypted.Name = "txt_decrypted";
+            this.txt_decrypted.Size = new System.Drawing.Size(158, 20);
+            this.txt_decrypted.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Decrypt message";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt_result
+            // 
+            this.txt_result.Enabled = false;
+            this.txt_result.Location = new System.Drawing.Point(147, 56);
+            this.txt_result.Name = "txt_result";
+            this.txt_result.Size = new System.Drawing.Size(158, 20);
+            this.txt_result.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter a message :";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(12, 83);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(345, 174);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(337, 148);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Demonstration tab";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 269);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_cipher;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox txt_result;
+        private System.Windows.Forms.TextBox txt_decrypted;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ NETLicense.IO exemple/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NETLicense.IO_exemple {
+    public partial class Form1 : Form {
+
+        /// <summary>
+        /// The very secret key
+        /// Mzrked to be a server variable
+        /// </summary>
+        [Obfuscation(Exclude = false, Feature = "server")]
+        public string key = "ThisIsAVerySecretKey";
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            /// We call a method marked to be a Remote method
+            txt_result.Text = CryptoHelper.Encrypt(txt_cipher.Text, key);
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            /// We call a method marked to be a Remote method
+            txt_decrypted.Text = CryptoHelper.Decrypt(txt_result.Text, key);
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+    }
+
+    /// <summary>
+    /// A simple crypto class, contingent
+    /// only for demonstration purposes
+    /// </summary>
+    public static class CryptoHelper {
+
+        /// <summary>
+        /// Marked to be a Remote method
+        /// </summary>
+        /// <param name="plainString">a string you want to encryt</param>
+        /// <param name="argKey">tge very secret key</param>
+        /// <returns>an encrypted string</returns>
+        [Obfuscation(Exclude = false, Feature = "remote")]
+        public static string Encrypt(string plainString, string argKey) {
+            var data = Encoding.UTF8.GetBytes(plainString);
+            var md5 = new MD5CryptoServiceProvider();
+            var key = md5.ComputeHash(Encoding.UTF8.GetBytes(argKey));
+            var tripleDes = new TripleDESCryptoServiceProvider() { Key = key, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 };
+            var transform = tripleDes.CreateEncryptor();
+            var resultsByteArray = transform.TransformFinalBlock(data, 0, data.Length);
+            return Convert.ToBase64String(resultsByteArray);
+        }
+
+        /// <summary>
+        /// Marked to be a Remote method
+        /// </summary>
+        /// <param name="encryptedString">an encrypted string you want to decrypt</param>
+        /// <param name="argKey">the very secret key</param>
+        /// <returns></returns>
+        [Obfuscation(Exclude = false, Feature = "remote")]
+        public static string Decrypt(string encryptedString, string argKey) {
+            var data = Convert.FromBase64String(encryptedString);
+            var md5 = new MD5CryptoServiceProvider();
+            var key = md5.ComputeHash(Encoding.UTF8.GetBytes(argKey));
+            var tripleDes = new TripleDESCryptoServiceProvider() { Key = key, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 };
+            var transform = tripleDes.CreateDecryptor();
+            var resultsByteArray = transform.TransformFinalBlock(data, 0, data.Length);
+            return Encoding.UTF8.GetString(resultsByteArray);
+        }
+    }
+}
